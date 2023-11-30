3e88f868cc187ffc270b7ed38a4c8d9160c05b4d
Jackie Chheng <jackiechheng@Jackies-MacBook-Pro.local>
2023-11-29 20:46:50 -0700

    added contact and leaderboard

--- server.js
+++ server.js
@@ -3,4 +3,29 @@ const path = require('path');
 
 const PORT = process.env.port || 3001;
 
+const app = express();
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/login', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/login.html'))
+);
+
+app.get('/minigames', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/minigames.html'))
+);
+
+app.get('/contact', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/contact.html'))
+);
+
+app.get('/leaderboard', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/leaderboard.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
