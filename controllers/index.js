8cf5275620e5ef08d21c3d0cae4562be97597f19
boomshakazulu <dallonlahoda@hotmail.com>
2023-11-30 20:50:42 -0700

    modified handlebars for first route render

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,14 @@
-const app = require('express').Router();
+const app = require("express").Router();
 
 // Imported modular routes
-const contactRouter = require('./contact'); 
-const homeRouter = require('./home');
-const lbRouter = require('./leaderboard');
-const gamesRouter = require('./minigames');
+const contactRouter = require("./contact");
+const homeRouter = require("./home");
+const lbRouter = require("./leaderboard");
+const gamesRouter = require("./minigames");
 
-app.use('/contact', contactRouter);
-app.use('/home', homeRouter);
-app.use('/leaderboard', lbRouter);
-app.use('/minigames', gamesRouter);
-
+app.use("/contact", contactRouter);
+app.use("/", homeRouter);
+app.use("/leaderboard", lbRouter);
+app.use("/minigames", gamesRouter);
 
 module.exports = app;
