7fba665d4b8cea8448a2773f52dee0ea492bc0c5
Shawn Crook <shawncrook411@gmail.com>
2023-11-30 19:16:48 -0700

    created first render route

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,35 @@
-const home = require('express').Router();
+const router = require('express').Router();
 // const { v4: uuidv4 } = require('uuid');
 
 
 
+// GET ROUTES
+router.get('/', async (req, res) => {
+    try{
+        res.render('homepage')
+    } catch (err) { console.log(err) }
+});
+
+// app.get('/login', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/login.html'))
+// );
+
+// app.get('/minigames', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/pages/minigames.html'))
+// );
+
+// app.get('/contact', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/contact.html'))
+// );
+
+// app.get('/leaderboard', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/leaderboard.html'))
+// );
+
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// );
 
 
-module.exports = home;
+
+module.exports = router;
--- server.js
+++ server.js
@@ -33,31 +33,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-//GET ROUTES
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/login', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/login.html'))
-);
-
-app.get('/minigames', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/minigames.html'))
-);
-
-app.get('/contact', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/contact.html'))
-);
-
-app.get('/leaderboard', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/leaderboard.html'))
-);
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening at localhost:${PORT}`));
 });
