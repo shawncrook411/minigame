01c1a21e8f671fe4f62c7b050b7fd2753427f529
Shawn Crook <shawncrook411@gmail.com>
2023-11-30 18:52:42 -0700

    fixed npm start

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,6 +1,6 @@
 const lb = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+// const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 
 
