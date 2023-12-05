c34f57688ee10db4b65347b10da367b04a428a1d
Shawn Crook <shawncrook411@gmail.com>
2023-12-04 20:32:22 -0700

    re organized controllers files to accomodate snake

--- controllers/contact.js
+++
@@ -1,8 +0,0 @@
-const contact = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-// const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
-
-
-
-
-module.exports = contact;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const app = require("express").Router();
 
 // Imported modular routes
-const contactRouter = require("./contact");
 const homeRouter = require("./home");
 const apiRouter = require("./api/index");
 
-app.use("/contact", contactRouter);
 app.use("/", homeRouter);
 app.use("/api", apiRouter);
 
